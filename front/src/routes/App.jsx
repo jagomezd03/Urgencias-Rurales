7b5eafcf793e8c628f82afe9e6704312db43cb2a
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-09-28 21:17:16 -0500

    Estrutura agregada del frontend

---
+++ front/src/routes/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const App = () => {
+	return (
+		<h1>Hola Mundo!</h1>
+	);
+}
+
+export default App;
